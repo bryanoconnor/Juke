0b9dd585b912b212efb9502035c551458818b486
jgiunta1 <jackgiunta@gmail.com>
2017-06-25 23:39:21 -0400

    Sign-in with Facebook/phone/email buttons implemented

--- app/src/main/java/com/studios/juke/juke/AuthUiActivity.java
+++ app/src/main/java/com/studios/juke/juke/AuthUiActivity.java
@@ -1,13 +1,38 @@
 package com.studios.juke.juke;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
 
 public class AuthUiActivity extends AppCompatActivity {
+
+    // Choose an arbitrary request code value
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth_ui);
+
+        // Check to see if signed in already
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // already signed in
+        }
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(
+                                Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                        new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build(),
+                                        new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build())
+                        )
+                        .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+                        .build(),
+        RC_SIGN_IN);
     }
 }
