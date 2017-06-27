3dce82dc3563cb1c35fbb3a8f89cd08991217792
jgiunta1 <jackgiunta@gmail.com>
2017-06-27 00:00:59 -0400

    Added Signed In Activity from FB Demo

--- app/src/main/java/com/studios/juke/juke/UserAuth/AuthUiActivity.java
+++ app/src/main/java/com/studios/juke/juke/UserAuth/AuthUiActivity.java
@@ -3,12 +3,17 @@ package com.studios.juke.juke.UserAuth;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.MainThread;
 import android.support.annotation.StringRes;
+import android.support.annotation.StyleRes;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.AuthUI.IdpConfig;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.ResultCodes;
@@ -16,22 +21,41 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.studios.juke.juke.BuildConfig;
 import com.studios.juke.juke.R;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class AuthUiActivity extends AppCompatActivity {
-
+    @BindView(R.id.sign_in)
+    Button mSignIn;
     // Choose an arbitrary request code value
-    private static final int RC_SIGN_IN = 123;
-    private static final String LOG_TAG= AuthUiActivity.class.getSimpleName();    @Override
+    private static final int RC_SIGN_IN = 100;
+    private static final String FIREBASE_TOS_URL = "https://firebase.google.com/terms/";
+    private static final String LOG_TAG= AuthUiActivity.class.getSimpleName();
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth_ui);
+        ButterKnife.bind(this);
 
         // Check to see if signed in already
+
         FirebaseAuth auth = FirebaseAuth.getInstance();
         if (auth.getCurrentUser() != null) {
-            // already signed in
+            startSignedInActivity(null);
+            finish();
+            return;
         }
+
+    }
+
+    @OnClick(R.id.sign_in)
+    public void signIn(View view) {
         startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
@@ -43,9 +67,8 @@ public class AuthUiActivity extends AppCompatActivity {
                         .setIsSmartLockEnabled(!BuildConfig.DEBUG)
                         .setLogo(R.mipmap.ic_launcher)
                         .build(),
-        RC_SIGN_IN);
+                RC_SIGN_IN);
     }
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -93,7 +116,26 @@ public class AuthUiActivity extends AppCompatActivity {
         startActivity(
                 SignedInActivity.createIntent(
                         this,
-                        response));
+                        response,
+                        new SignedInActivity.SignedInConfig(
+                                getSelectedLogo(),
+                                getSelectedTheme(),
+                                getSelectedProviders(),
+                                FIREBASE_TOS_URL,
+                                false,
+                                true)));
+    }
+
+    @MainThread
+    @StyleRes
+    private int getSelectedTheme() {
+        return AuthUI.getDefaultTheme();
+    }
+
+    @MainThread
+    @DrawableRes
+    private int getSelectedLogo() {
+        return R.mipmap.ic_launcher;
     }
 
     @MainThread
@@ -106,4 +148,30 @@ public class AuthUiActivity extends AppCompatActivity {
         in.setClass(context, AuthUiActivity.class);
         return in;
     }
+
+    @MainThread
+    private List<IdpConfig> getSelectedProviders() {
+        List<IdpConfig> selectedProviders = new ArrayList<>();
+
+        selectedProviders.add(
+                new IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER)
+                        .setPermissions(getFacebookPermissions())
+                        .build());
+        selectedProviders.add(new IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build());
+        selectedProviders.add(
+                new IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build());
+
+        return selectedProviders;
+    }
+
+    @MainThread
+    private List<String> getFacebookPermissions() {
+        List<String> result = new ArrayList<>();
+        result.add("user_friends");
+        result.add("user_photos");
+
+        return result;
+    }
+
+
 }
