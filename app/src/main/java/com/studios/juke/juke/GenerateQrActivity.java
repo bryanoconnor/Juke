2edb408723376d5152d3114043a9ff51b2c9e712
bryanoconnor <bryan.oconnor0426@gmail.com>
2017-06-27 01:40:50 -0400

    implemented QR code

--- app/src/main/java/com/studios/juke/juke/GenerateQrActivity.java
+++ app/src/main/java/com/studios/juke/juke/GenerateQrActivity.java
@@ -1,8 +1,16 @@
 package com.studios.juke.juke;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 public class GenerateQrActivity extends MenuBarOptions {
 
@@ -15,5 +23,22 @@ public class GenerateQrActivity extends MenuBarOptions {
 
         mToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(mToolbar);
+
+        QRCodeWriter writer = new QRCodeWriter();
+        try {
+            BitMatrix bitMatrix = writer.encode("Yo", BarcodeFormat.QR_CODE, 512, 512);
+            int width = bitMatrix.getWidth();
+            int height = bitMatrix.getHeight();
+            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                }
+            }
+            ((ImageView) findViewById(R.id.qr_image)).setImageBitmap(bmp);
+
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
     }
 }
