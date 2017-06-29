8903170f31796da8761d11e22879884835fa685d
jgiunta1 <jackgiunta@gmail.com>
2017-06-29 17:36:47 -0400

    Implemented SpotifySearchLoader class

---
+++ app/src/main/java/com/studios/juke/juke/SpotifySearchLoader.java
@@ -0,0 +1,40 @@
+package com.studios.juke.juke;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import android.util.Log;
+
+import java.util.List;
+
+/**
+ * Created by Jack on 6/29/2017.
+ */
+
+public class SpotifySearchLoader extends AsyncTaskLoader<List<Song>> {
+
+    private String mUrl;
+    private String mAccessToken;
+    public static final String LOG_TAG = "SPOTLOADER";
+
+    public SpotifySearchLoader(Context context, String url, String accessToken) {
+        super(context);
+        mUrl = url;
+        mAccessToken = accessToken;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        Log.i(LOG_TAG, "onStartLoading() Called");
+        forceLoad();
+    }
+
+    @Override
+    public List<Song> loadInBackground() {
+        if(mUrl == null)
+            return null;
+
+        List<Song> result = QueryUtils.fetchSongData(mUrl, mAccessToken);
+        Log.i(LOG_TAG, "loadInBackground() Finished");
+        return result;
+    }
+}
