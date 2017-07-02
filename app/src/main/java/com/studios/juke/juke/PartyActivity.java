bbc7c3d47aabb91be000c34ff2ad9649a2e81f3b
jgiunta1 <jackgiunta@gmail.com>
2017-07-01 21:34:05 -0400

    Added SongAdapter

--- app/src/main/java/com/studios/juke/juke/PartyActivity.java
+++ app/src/main/java/com/studios/juke/juke/PartyActivity.java
@@ -1,13 +1,48 @@
 package com.studios.juke.juke;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class PartyActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "PartyActivity";
+
+    public static final String ANONYMOUS = "anonymous";
+    public static final int RC_PHOTO_PICKER = 2;
+    private String mUsername;
+
+    // Firebase instance variables
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mPartyDatabaseReference;
+    private ChildEventListener mChildEventListener;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @BindView(R.id.partyListView)
+    ListView mSongListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_party);
+        ButterKnife.bind(this);
+
+
+        // Initialize Firebase components
+        mUsername = ANONYMOUS;
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mPartyDatabaseReference = mFirebaseDatabase.getReference().child("messages");
+
+
     }
 }
