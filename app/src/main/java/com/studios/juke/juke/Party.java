b838f4a6c2578c1f3ca3f46bc35fdccfeadd5d54
jgiunta1 <jackgiunta@gmail.com>
2017-07-03 22:18:26 -0400

    Implemented JoinPartyActivty

--- app/src/main/java/com/studios/juke/juke/Party.java
+++ app/src/main/java/com/studios/juke/juke/Party.java
@@ -1,21 +1,31 @@
 package com.studios.juke.juke;
 
+import java.io.Serializable;
+
 /**
  * Created by Jack on 7/2/2017.
  */
 
-public class Party {
+public class Party implements Serializable{
 
     private String mSongs;
     private String mMembers;
+    private String mID;
 
     public Party(){
 
     }
 
-    public Party(String songs, String members){
+    public Party(String id){
+        mMembers = "DEFAULT";
+        mSongs = "DEFAULT";
+        mID = id;
+    }
+
+    public Party(String songs, String members, String id){
         mMembers = members;
         mSongs = songs;
+        mID = id;
     }
 
     public String getmSongs() {
